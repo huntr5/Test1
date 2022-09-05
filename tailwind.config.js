fcf72135e432a31f8742875493a7140c316c3020
huntr5 <sidharthachatterjee2016@gmail.com>
2022-09-05 22:12:19 +0530

    a commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,35 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require('daisyui'),
-      ],
-    
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                "primary": "#570DF8",
-                "secondary": "#F000B8",
-                "accent": "#37CDBE",
-                "neutral": "#3D4451",
-                "base-100": "#111111",
-                "info": "#3ABFF8",
-                "success": "#36D399",
-                "warning": "#FBBD23",
-                "error": "#F87272",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        colors:{
+            "primary": "#570DF8",
+            "secondary": "#F000B8",
+            "accent": "#37CDBE",
+            "neutral": "#3D4451",
+            "base-100": "#111111",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
             },
-          },
-        ],
-    },  
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+
+  daisyui: {
+    styled : true,
+    base : false,
+    themes: [
+        {
+            dark: {},
+        },
+
+    ],
+        
+  },
+  
 };
-
-
